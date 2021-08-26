2272b68dc5459a1a7cb66e93f08d196e42b31bfb
Ben Leach <benleach@pm.me>
2021-08-25 20:37:20 -0400

    resolve conflicts

--- testpr.sh
+++ testpr.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-hub --version >> /dev/null
+gh --version >> /dev/null
 
 if [ $? -ne 0 ]; then
-		echo "Hub not found. Install from https://cli.github.com/"
+		echo "GitHub CLI not found. Install from https://cli.github.com/"
 		exit 1
 fi
 
-hub pr checkout $1
+gh pr checkout $1
 npm run test
 
 if [ $? -eq 0 ]; then
-		hub pr merge $1
+		gh pr merge $1
 		echo "SUCCESS"
 else
 		echo "FAILED..."
 fi
+
