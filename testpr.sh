ac0cd54a25243639f2da52c0b36fdc47b7f6bcf3
Ben Leach <benleach@pm.me>
2021-08-26 00:44:13 -0400

    Update test script

--- testpr.sh
+++ testpr.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-gh --version >> /dev/null
 
-if [ $? -ne 0 ]; then
+if ! gh --version >> /dev/null; then
 		echo "GitHub CLI not found. Install from https://cli.github.com/"
 		exit 1
 fi
 
-gh pr checkout $1
-npm run test
+if ! gh pr diff $2 | grep -q "_data/semesters/$1"; then
+		gh pr comment -b "Your JSON is in the wrong folder, please move it to the $1 folder"
+		echo "PR FAILED, JSON in wrong folder."
+		exit 1
+fi
 
-if [ $? -eq 0 ]; then
-		gh pr merge -dm $1
-		echo "SUCCESS"
+gh pr checkout $2
+
+if npm run test jest > /dev/null 2>&1; then
+		gh pr merge -dm $2
+		echo "PR PASSED checks"
 else
-		gh pr comment -b "Please check your JSON, it doesn't appear to be valid. https://jsonlint.com/" $1
-		echo "FAILED..."
+		gh pr comment -b "Please check your JSON, it doesn't appear to be valid. https://jsonlint.com/" $2
+		echo "PR FAILED, invalid JSON"
 fi
 
 git checkout master
-
