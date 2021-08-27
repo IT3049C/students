531271b8e3b03a322960419575f9dfba1100463f
Ben Leach <benleach@pm.me>
2021-08-27 11:30:25 -0400

    testing

--- testpr.sh
+++ testpr.sh
@@ -6,7 +6,7 @@ if ! gh --version > /dev/null 2>&1; then
 fi
 
 if ! gh pr diff $2 | grep -q "_data/semesters/$1"; then
-		gh pr comment -b "Your JSON is in the wrong folder, please move it to the $1 folder"
+#		gh pr comment -b "Your JSON is in the wrong folder, please move it to the $1 folder"
 		echo "PR FAILED, JSON in wrong folder."
 		exit 1
 fi
@@ -14,10 +14,10 @@ fi
 gh pr checkout $2
 
 if npm run test jest > /dev/null 2>&1; then
-		gh pr merge -dm $2
+#		gh pr merge -dm $2
 		echo "PR PASSED checks"
 else
-		gh pr comment -b "Please check your JSON, it doesn't appear to be valid. https://jsonlint.com/" $2
+#		gh pr comment -b "Please check your JSON, it doesn't appear to be valid. https://jsonlint.com/" $2
 		echo "PR FAILED, invalid JSON"
 fi
 
